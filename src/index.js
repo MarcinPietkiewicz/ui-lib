4f6e64fa5cabf6cd599b8a47e069b6f6ab980cbd
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-09-18 00:30:09 +0200

    Added a dropdown element class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 // import './test.css'
 
 // console.log('test');
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
-
 tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
